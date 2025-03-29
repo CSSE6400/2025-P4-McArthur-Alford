33e964c0aaf2f14c37ce9f3298d3c2e0b50b9117
mcarthur <mcarthur.alford@proton.me>
2025-03-29 09:44:12 +0000

    things

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [ pkgs.terraform ];
+        shellHook = '''';
+      };
+    };
+}
